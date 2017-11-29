9aef99998a7878c56462291936b3ddec0376ed63
Vitor Finotti Ferreira <vfinotti@gmail.com>
2017-11-29 00:25:12 -0200

    Fixing csv download links

--- download_files.sh
+++ download_files.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 wget "http://dados.prefeitura.sp.gov.br/dataset/0aecfa2b-aa3a-40d4-8183-0d4351b7fd0a/resource/de1e9b0a-e185-4e0f-9d95-d25d1bebbd36/download/sp15620151-semestre.csv"
-wget "http://dados.prefeitura.sp.gov.br/dataset/0aecfa2b-aa3a-40d4-8183-0d4351b7fd0a/resource/de1e9b0a-e185-4e0f-9d95-d25d1bebbd36/download/sp15620152-semestre.csv"
-wget "http://dados.prefeitura.sp.gov.br/dataset/0aecfa2b-aa3a-40d4-8183-0d4351b7fd0a/resource/de1e9b0a-e185-4e0f-9d95-d25d1bebbd36/download/sp15620161-semestre.csv"
-wget "http://dados.prefeitura.sp.gov.br/dataset/0aecfa2b-aa3a-40d4-8183-0d4351b7fd0a/resource/de1e9b0a-e185-4e0f-9d95-d25d1bebbd36/download/sp15620162-semestre.csv"
-wget "http://dados.prefeitura.sp.gov.br/dataset/0aecfa2b-aa3a-40d4-8183-0d4351b7fd0a/resource/de1e9b0a-e185-4e0f-9d95-d25d1bebbd36/download/sp15620171-semestre.csv"
-wget "http://dados.prefeitura.sp.gov.br/dataset/0aecfa2b-aa3a-40d4-8183-0d4351b7fd0a/resource/de1e9b0a-e185-4e0f-9d95-d25d1bebbd36/download/sp15620172-semestre.csv"
+wget "http://dados.prefeitura.sp.gov.br/dataset/0aecfa2b-aa3a-40d4-8183-0d4351b7fd0a/resource/c3394c98-d5f1-4202-85a1-af174d86a38a/download/sp15620152-semestre.csv"
+wget "http://dados.prefeitura.sp.gov.br/dataset/0aecfa2b-aa3a-40d4-8183-0d4351b7fd0a/resource/3a5ff4b6-d6b3-458c-a050-04af05a171ee/download/sp15620161-semestre.csv"
+wget "http://dados.prefeitura.sp.gov.br/dataset/0aecfa2b-aa3a-40d4-8183-0d4351b7fd0a/resource/c3ef2030-77ec-4eed-945b-297fd52459d1/download/sp15620162-semestre.csv"
+wget "http://dados.prefeitura.sp.gov.br/dataset/0aecfa2b-aa3a-40d4-8183-0d4351b7fd0a/resource/542c3405-5a2d-4e76-b318-92ab4869d453/download/sp15620171-semestre.csv"
+wget "http://dados.prefeitura.sp.gov.br/dataset/0aecfa2b-aa3a-40d4-8183-0d4351b7fd0a/resource/c439343b-6e2e-4cc5-84fe-aba0e54688a1/download/sp15620172-semestre.csv"
